2822429212e41752ce303dee46f2bd88e78cfa21
yangww <1483722265@qq.com>
2019-05-17 14:49:03 +0800

    gitignore test step1

---
+++ GitMethodTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitMethodTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("for git test");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
